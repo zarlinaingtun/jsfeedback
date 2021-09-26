a50ba5212fee591918d29cbfb17a322f7a7994e9
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-09-25 18:10:57 -0700

    first

---
+++ js/app.js
@@ -0,0 +1,84 @@
+const panel = document.querySelector('#panel');
+
+const ratingcontainer = document.querySelector('.ratings-container');
+
+const ratings = document.querySelectorAll('.rating');
+
+const sendbtn = document.querySelector('#send');
+
+
+let selectedrating='Satisfied';
+ratingcontainer.addEventListener('click',(e)=>{
+    // if(e.target.classList.contains('rating')){
+    //     console.log(e.target);
+    // }
+    if(e.target.parentNode.classList.contains("rating")){
+        // console.log(e.target);
+        removeactive();
+        e.target.parentNode.classList.add('active');
+
+        // selectedrating=e.target.nextElementSibling.textContent;
+       
+        selectedrating=e.target.parentNode.lastElementChild.textContent;
+        console.log(selectedrating);
+
+    }
+    else if(e.target.classList.contains('rating')){
+        removeactive();
+        e.target.classList.add('active');
+        selectedrating=e.target.lastElementChild.textContent;
+    }
+
+});
+
+function removeactive(){
+    // ratings.forEach(rating=>{
+    //     rating.classList.remove('active');
+    // });
+    for(let i=0;i<ratings.length;i++){
+        ratings[i].classList.remove('active');
+    }
+};
+
+sendbtn.addEventListener('click', () => {
+    panel.innerHTML=`
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <strong>Feedback : ${selectedrating}</strong>
+    <p>We'll use your feedback to imporve our customer</p>`;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
